0ee775a587058b5622c9c303f182f4f30f4b8265
Borislav16 <borko.g16@gmail.com>
2024-09-18 08:17:10 +0300

    Create home.js

---
+++ JS Applications/04.ExerciseSinglePageApplications/01.Forum/javascript/home.js
@@ -0,0 +1,8 @@
+import { elements } from "./elements.js";
+import { loadTopics } from "./topic.js";
+
+export function loadHome(e) {
+    if (e) e.preventDefault();
+    elements.homeForm.style.display = 'block';
+    loadTopics();
+}
