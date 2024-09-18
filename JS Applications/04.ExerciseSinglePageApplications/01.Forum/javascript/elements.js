7a07d588ac5ed22b63ef7c0bcec852ac2b934938
Borislav16 <borko.g16@gmail.com>
2024-09-18 08:17:07 +0300

    Create elements.js

---
+++ JS Applications/04.ExerciseSinglePageApplications/01.Forum/javascript/elements.js
@@ -0,0 +1,12 @@
+export const elements = {
+    homeBtn: document.querySelector('#home-btn'),
+    cancelBtn: document.querySelector('.cancel'),
+    postBtn: document.querySelector('.public'),
+    titleEl: document.querySelector('#topicName'),
+    userEl: document.querySelector('#username'),
+    postTextEl: document.querySelector('#postText'),
+    topicCtr: document.querySelector('.topic-container'),
+    homeForm: document.querySelector('.new-topic-border'),
+    mainEl: document.querySelector('main'),
+    
+};
